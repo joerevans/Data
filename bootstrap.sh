4e100c5abf154a941684dd806cdbc3435abf618a
Joseph Evans <jevans@zdatainc.com>
2017-04-05 12:26:32 -0600

    added bootstrap automation, fixed memory allocation for vagrant file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,14 @@ echo 'vm.swappiness = 0' >> /etc/sysctl.conf
 systemctl stop firewalld
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
-yum install wget 
-rpm --install /vagrant/couchbase-server-community-4.5.0-centos7.x86_64.rpm
+yum install wget -y
+
+# Download version of couch base
+wget https://packages.couchbase.com/releases/4.5.0/couchbase-server-community-4.5.0-centos7.x86_64.rpm
+
+# Install couchbase
+rpm --install couchbase-server-community-4.5.0-centos7.x86_64.rpm
+
+# insert path int bas_profile line 10
+echo 'PATH=$PATH:$HOME/bin:/opt/couchbase/bin' >> ~/.bash_profile
+echo 'PATH=$PATH:$HOME/bin:/opt/couchbase/bin' >> /home/vagrant/.bash_profile
